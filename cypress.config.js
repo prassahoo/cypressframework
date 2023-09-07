0e45f1fd1ef9d3d80f709675bba86f5af6152dfd
Prasanta Kumar Sahoo <prassahoo@deloitte.com>
2023-09-07 17:45:32 +0530

    test all js files

--- cypress.config.js
+++ cypress.config.js
@@ -26,8 +26,8 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents,
-    //specPattern: 'cypress/integration/examples/*.js'
-    specPattern: 'cypress/integration/examples/BDD/*.feature'
+    specPattern: 'cypress/integration/examples/*.js'
+    //specPattern: 'cypress/integration/examples/BDD/*.feature'
   },
 });
 
